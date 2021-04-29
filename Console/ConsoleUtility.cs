f4534c6af3fe00ac13fd57a51ef1becd24655ba9
Chris Heller <chris.heller.work@gmail.com>
2021-04-29 16:50:19 +0200

    ConsoleUtility no longer executes automatically but may be called from outside to instantiate a new console

--- Console/ConsoleUtility.cs
+++ Console/ConsoleUtility.cs
@@ -1,24 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace ConsoleUtility
 {
-    static class ConsoleUtility
+    public static class ConsoleUtility
     {
-#if ENABLE_LEGACY_INPUT_MANAGER
-        const string kPrefabName = "Console_LegacyInput";
-#elif ENABLE_INPUT_SYSTEM
-        const string kPrefabName = "Console_NewInputSystem";
-#endif
-        [RuntimeInitializeOnLoadMethod]
-        static void CreateConsole()
+        public const string LegacyConsoleName = "Console_LegacyInput";
+        public const string UnityInputSystemConsoleName = "Console_NewInputSystem";
+
+
+        /// <summary>
+        /// You may call this method to instantiate the console manually.
+        /// </summary>
+        /// <param name="consoleName">The name of the prefab.</param>
+        /// <param name="consoleGo">The instantiated prefab reference.</param>
+        /// <returns>A <see cref="bool"/> result whether the console object could be instantiated or not.</returns>
+        public static bool CreateConsole(string consoleName, out GameObject consoleGo)
         {
-            var prefab = GameObject.Instantiate(Resources.Load<GameObject>(kPrefabName));
-            prefab.name = "Console";
-            GameObject.DontDestroyOnLoad(prefab);
-            
+            if (string.IsNullOrWhiteSpace(consoleName))
+            {
+                Debug.LogError($"{nameof(CreateConsole)}: Invalid prefab name!");
+                consoleGo = default;
+                return false;
+            }
+
+            GameObject prefabInstance = GameObject.Instantiate(Resources.Load<GameObject>(consoleName));
+            prefabInstance.name = "Console";
+
+            consoleGo = prefabInstance;
+
+            return consoleGo != null;
         }
     }
 }
-
